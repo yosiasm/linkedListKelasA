c48035ab2e136890c68afaf1366e6736619d93a4
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2016-09-27 11:39:07 +0700

    The beginning

---
+++ CobaLinkedList.java
@@ -0,0 +1,82 @@
+package cobalinkedlist;
+
+/**
+ *
+ * @author Pemrograman Dasar II Stiki, kelas A
+ */
+
+class Node{
+    String data;
+    Node prev;
+    Node next;
+    public Node(String new_data){
+        this.data = new_data;
+    }
+    public void setNext(Node otherNode){
+        this.next = otherNode;
+        otherNode.prev = this;
+    }
+    public void setPrev(Node otherNode){
+        this.prev = otherNode;
+        otherNode.next = this;
+    }
+}
+
+class LinkedList{
+    Node head;
+    Node tail;
+    public LinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+    public void push(Node newNode){ // 5 orang
+        // TODO: write the code, add newNode
+    }
+    public Node qpop(){ // 5 orang
+        // TODO: write the code, pop node (queue), return it
+        return null;
+    }
+    public Node spop(){ // 5 orang
+        // TODO: write the code, pop node (stack), return it
+        return null;
+    }
+    public void insert(Node newNode, Node position){ // 6 orang
+        // TODO: insert newNode after position
+    }
+    public void delete(Node position){ // 6 orang
+        // TODO: delete Node position from linked list
+    }
+    public void view(){
+        Node current = head;
+        while(current != null){            
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println("");
+    }
+}
+public class CobaLinkedList {
+    public static void main(String[] args) {
+        LinkedList kruntelan = new LinkedList();
+        kruntelan.push(new Node("A"));
+        kruntelan.push(new Node("B"));
+        kruntelan.view(); // muncul A B
+        kruntelan.push(new Node("C"));
+        kruntelan.qpop();
+        kruntelan.view(); // muncul B C
+        kruntelan.spop(); // muncul B
+        kruntelan.spop(); // muncul ''
+        Node x = new Node("X");
+        Node y = new Node("Y");
+        kruntelan.push(new Node("A"));
+        kruntelan.push(x);
+        kruntelan.push(new Node("B"));
+        kruntelan.push(new Node("C"));
+        kruntelan.insert(y, x);
+        kruntelan.view(); // A X Y B C
+        kruntelan.delete(x);
+        kruntelan.view(); // A Y B C
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# LinkedListKelasA
+
+File ini menggunakan syntax `markdown`
+
+# Programmers
+* Skeleton:
+    - Go Frendi (https://github.com/goFrendiAsgard)
+* `push(Node newNode)` 5 orang
+    - Nama, NRP (url ke github profile)
+* `qpop()` 5 orang
+* `spop()` 5 orang
+* `insert(Node newNode, Node position)` 6 orang
+* `delete(Node position)` // 6 orang
